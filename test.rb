daeb1884e825c3404e8060e7081904cfc8505571
Luuk Veenis <luukveenis@shaw.ca>
2015-11-30 18:24:57 -0800

    Add simple tests to verify parser is working

---
+++ test.rb
@@ -0,0 +1,35 @@
+require './parse_tree'
+
+def assert_true val
+  raise "Test failed with val: #{val}" unless val
+  puts "Success: #{val}"
+end
+
+expr1 = "( ^ ( ! A ) B )"
+tree1 = ParseTree.new(expr1.split)
+expr2 = "A"
+tree2 = ParseTree.new(expr2.split)
+expr3 = "( -> ( ^ A B ) ( v C ( ! B ) )"
+tree3 = ParseTree.new(expr3.split)
+
+# Tree 1
+assert_true(tree1.root.val == "^")
+assert_true(tree1.root.left.val == "!")
+assert_true(tree1.root.left.left.val == "A")
+assert_true(tree1.root.right.val == "B")
+
+# Tree 2
+assert_true(tree2.root.val == "A")
+assert_true(tree2.root.left.nil?)
+assert_true(tree2.root.right.nil?)
+
+# Tree 3
+assert_true(tree3.root.val == "->")
+assert_true(tree3.root.left.val == "^")
+assert_true(tree3.root.left.left.val == "A")
+assert_true(tree3.root.left.right.val == "B")
+assert_true(tree3.root.right.val == "v")
+assert_true(tree3.root.right.left.val == "C")
+assert_true(tree3.root.right.right.val == "!")
+assert_true(tree3.root.right.right.left.val == "B")
+assert_true(tree3.root.right.right.right.nil?)
